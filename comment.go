1a2e23af3f6de9aa8bd543f03d19b555cdbd95bc
shushuTona <42406967+shushuTona@users.noreply.github.com>
2024-11-04 02:39:10 +0900

    add comment.go

---
+++ comment.go
@@ -0,0 +1,47 @@
+package calc
+
+import "fmt"
+
+const (
+	CONST_1 = 100 // 定数1
+	CONST_2 = 200 // 定数2
+	CONST_3 = 300 // 定数3
+)
+
+// グローバル変数
+var GlobalVariable = 100
+
+// グローバル関数
+func GlobalFunc() {
+	fmt.Println("GlobalFunc")
+}
+
+// CommentStruct
+// コメント記載用の構造体
+//
+// # 見出しを記載する際は前後に改行が必要
+//
+//   - リスト1
+//   - リスト2
+//   - リスト3
+//
+// コードブロック
+//
+//	func HogeFunc(num int) {
+//		fmt.Println(num)
+//	}
+//
+// 通常リンク
+//
+// https://github.com/shushuTona/try_godoc
+//
+// ドキュメントリンク
+//
+// [Calc.Add]
+type CommentStruct struct {
+	// 公開フィールド
+	PublicField string
+
+	// 非公開フィールド
+	privateField string
+}
